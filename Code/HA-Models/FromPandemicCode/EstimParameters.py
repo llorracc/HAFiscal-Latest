bc84839d4054c3e708b840ed76d36becbeedb504
Håkon Tretvoll <hakon@tretvoll.com>
2021-09-24 14:40:25 +0200

    Added code for the esimation of discount factor distributions

---
+++ Code/HA-Models/FromPandemicCode/EstimParameters.py
@@ -0,0 +1,243 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+import csv
+from HARK.distribution import Uniform
+from importlib import reload
+
+
+#figs_dir = './Figures/FullRun_June7th/'
+figs_dir = './Figures/Hakon_test/'
+
+try:
+    os.mkdir(figs_dir)
+except OSError:
+    print ("Creation of the directory %s failed" % figs_dir)
+else:
+    print ("Successfully created the directory %s " % figs_dir)
+
+# Targets in the estimation of the discount factor distributions for each 
+# education level. 
+# From SCF 2004: [20,40,60,80]-percentiles of the Lorenz curve for liquid wealth
+data_LorenzPts_d = [0, 0.01, 0.60, 3.58]    # \
+data_LorenzPts_h = [0.06, 0.63, 2.98, 11.6] # -> units: % 
+data_LorenzPts_c = [0.15, 0.92, 3.27, 10.3] # /
+data_LorenzPts = [data_LorenzPts_d, data_LorenzPts_h, data_LorenzPts_c]
+data_LorenzPtsAll = np.array([0.03, 0.35, 1.84, 7.42])
+# From SCF 2004: Average liquid wealth to permanent income ratio 
+data_avgLWPI = np.array([15.7, 47.7, 111]) # weighted average of fractions in percent
+# From SCF 2004: Total LW over total PI by education group
+data_LWoPI = np.array([28.1, 59.6, 162]) # units: %
+
+# Population share of each type
+data_EducShares = [0.093, 0.527, 0.38] # Proportion of dropouts, HS grads, college types, SCF 2004 
+# Wealth share of each type 
+data_WealthShares = np.array([0.008, 0.179, 0.812])*100 # Percentage of total wealth of dropouts, HS grads, college types, SCF 2004 
+
+# Parameters concerning the distribution of discount factors
+# Initial values for estimation, taken from pandemic paper
+DiscFacMeanD = 0.9637   # Mean intertemporal discount factor for dropout types
+DiscFacMeanH = 0.9705   # Mean intertemporal discount factor for high school types
+DiscFacMeanC = 0.97557  # Mean intertemporal discount factor for college types
+DiscFacInit = [DiscFacMeanD, DiscFacMeanH, DiscFacMeanC]
+DiscFacSpread = 0.0253  # Half-width of uniform distribution of discount factors
+
+# Define the distribution of the discount factor for each eduation level
+DiscFacCount = 7
+DiscFacDstnD = Uniform(DiscFacMeanD-DiscFacSpread, DiscFacMeanD+DiscFacSpread).approx(DiscFacCount)
+DiscFacDstnH = Uniform(DiscFacMeanH-DiscFacSpread, DiscFacMeanH+DiscFacSpread).approx(DiscFacCount)
+DiscFacDstnC = Uniform(DiscFacMeanC-DiscFacSpread, DiscFacMeanC+DiscFacSpread).approx(DiscFacCount)
+DiscFacDstns = [DiscFacDstnD, DiscFacDstnH, DiscFacDstnC]
+
+# Parameters concerning Markov transition matrix
+# Normal
+Urate_normal = 0.05          # Unemployment rate in normal times
+Uspell_normal = 1.5          # Average duration of unemployment spell in normal times, in quarters
+UBspell_normal = 2           # Average duration of unemployment benefits in normal times, in quarters
+
+Splurge = 0.15 #0.32      # amount of income that is splurged
+
+# Basic model parameters: CRRA, growth factors, unemployment parameters (for normal times)
+CRRA = 1.0              # Coefficient of relative risk aversion
+PopGroFac = 1.0         #1.01**0.25  # Population growth factor
+PermGroFacAgg = 1.0     #1.01**0.25 # Technological growth rate or aggregate productivity growth factor
+IncUnemp = 0.3              # Unemployment benefits replacement rate (proportion of permanent income)
+IncUnempNoBenefits = 0.05   # Unemployment income when benefits run out (proportion of permanent income)
+
+# Parameters concerning the initial distribution of permanent income 
+pLvlInitMeanD = np.log(5.0)  # Average quarterly permanent income of "newborn" HS dropout ($1000)
+pLvlInitMeanH = np.log(7.5)  # Average quarterly permanent income of "newborn" HS graduate ($1000)
+pLvlInitMeanC = np.log(12.0) # Average quarterly permanent income of "newborn" HS  ($1000)
+pLvlInitStd = 0.4           # Standard deviation of initial log permanent income 
+
+# Parameters concerning grid sizes: assets, permanent income shocks, transitory income shocks
+aXtraMin = 0.001        # Lowest non-zero end-of-period assets above minimum gridpoint
+aXtraMax = 40           # Highest non-zero end-of-period assets above minimum gridpoint
+aXtraCount = 48         # Base number of end-of-period assets above minimum gridpoints
+aXtraExtra = [0.002,0.003] # Additional gridpoints to "force" into the grid
+aXtraNestFac = 3        # Exponential nesting factor for aXtraGrid (how dense is grid near zero)
+PermShkCount = 7        # Number of points in equiprobable discrete approximation to permanent shock distribution
+TranShkCount = 7        # Number of points in equiprobable discrete approximation to transitory shock distribution
+
+
+# Size of simulations
+AgentCountTotal = 50000 # Total simulated population
+T_sim = 80              # Number of quarters to simulate in counterfactuals
+
+# Basic lifecycle length parameters (don't touch these)
+T_cycle = 1
+
+# Define grid of aggregate assets to labor
+CgridBase = np.array([0.8, 1.0, 1.2])  
+
+num_base_MrkvStates = 2 + UBspell_normal #employed, unemployed with 2 quarters benefits, unemployed with 1 quarter benefit, unemployed no benefits
+
+def small_MrkvArray(e,u,ub,transition_ub=True):
+    small_MrkvArray = np.zeros((ub+2, ub+2))
+    small_MrkvArray[0,0] = e
+    small_MrkvArray[0,1] = 1-e
+    for i in np.array(range(ub))+1:
+        if transition_ub:
+            small_MrkvArray[i,i+1] = u
+        else:
+            small_MrkvArray[i,i] = u
+        small_MrkvArray[i,0] = 1-u
+    small_MrkvArray[ub+1,ub+1] = u
+    small_MrkvArray[ub+1,0] = 1-u
+    return small_MrkvArray 
+
+def makeCondMrkvArrays_base(Urate_normal, Uspell_normal, UBspell_normal):
+    U_persist_normal = 1.-1./Uspell_normal
+    E_persist_normal = 1.-Urate_normal*(1.-U_persist_normal)/(1.-Urate_normal)
+    MrkvArray_normal         = small_MrkvArray(E_persist_normal,    U_persist_normal,    UBspell_normal)
+    CondMrkvArrays = [MrkvArray_normal]
+    return CondMrkvArrays
+
+def makeFullMrkvArray(MacroMrkvArray, CondMrkvArrays):
+    for i in range(MacroMrkvArray.shape[0]):
+        this_row = MacroMrkvArray[i,0]*CondMrkvArrays[0]
+        for j in range(MacroMrkvArray.shape[0]-1):
+            this_row = np.concatenate((this_row, MacroMrkvArray[i,j+1]*CondMrkvArrays[j+1]),axis=1)
+        if i==0:
+            FullMrkv = this_row
+        else:
+            FullMrkv = np.concatenate((FullMrkv, this_row), axis=0)
+    return [FullMrkv]
+
+MacroMrkvArray_base = np.array([[1.0]])
+CondMrkvArrays_base = makeCondMrkvArrays_base(Urate_normal, Uspell_normal, UBspell_normal)
+MrkvArray_base = makeFullMrkvArray(MacroMrkvArray_base, CondMrkvArrays_base)
+
+
+# Define permanent income growth rates
+PermGroFac_base =   [1.0]
+
+# Define the permanent and transitory shocks 
+TranShkStd = [0.1]
+PermShkStd = [0.05]
+Rfree_base = [1.01]
+
+LivPrb_base     = [1.0-1/240.0]
+# find intial distribution of states
+vals, vecs = np.linalg.eig(np.transpose(MrkvArray_base[0]))
+dist = np.abs(np.abs(vals) - 1.)
+idx = np.argmin(dist)
+init_mrkv_dist = vecs[:,idx].astype(float)/np.sum(vecs[:,idx].astype(float))
+
+
+# Define a parameter dictionary for dropout type
+init_dropout = {"cycles": 0,
+                "T_cycle": T_cycle,
+                'T_sim': 400, #Simulate up to age 400
+                'T_age': None,
+                'AgentCount': 10000,
+                "PermGroFacAgg": PermGroFacAgg,
+                "PopGroFac": PopGroFac,
+                "CRRA": CRRA,
+                "DiscFac": 0.98, # This will be overwritten at type construction
+                "Rfree_base" : Rfree_base,
+                "PermGroFac_base": PermGroFac_base,
+                "LivPrb_base": LivPrb_base,
+                "Rfree" : np.array(num_base_MrkvStates*Rfree_base),
+                "PermGroFac": [np.array(PermGroFac_base*num_base_MrkvStates)],
+                "LivPrb": [np.array(LivPrb_base*num_base_MrkvStates)],
+                "MrkvArray_base" : MrkvArray_base, 
+                "MacroMrkvArray_base" : MacroMrkvArray_base,
+                "CondMrkvArrays_base" : CondMrkvArrays_base,
+                "MrkvArray" : MrkvArray_base, 
+                "MacroMrkvArray" : MacroMrkvArray_base,
+                "CondMrkvArrays" : CondMrkvArrays_base,
+                "BoroCnstArt": 0.0,
+                "PermShkStd": PermShkStd,
+                "PermShkCount": PermShkCount,
+                "TranShkStd": TranShkStd,
+                "TranShkCount": TranShkCount,
+                "UnempPrb": 0.0, # Unemployment is modeled as a Markov state
+                "IncUnemp": IncUnemp,
+                "IncUnempNoBenefits": IncUnempNoBenefits,
+                "aXtraMin": aXtraMin,
+                "aXtraMax": aXtraMax,
+                "aXtraCount": aXtraCount,
+                "aXtraExtra": aXtraExtra,
+                "aXtraNestFac": aXtraNestFac,
+                "CubicBool": False,
+                "vFuncBool": False,
+                'aNrmInitMean': np.log(0.00001), # Initial assets are zero
+                'aNrmInitStd': 0.0,
+                'pLvlInitMean': pLvlInitMeanD,
+                'pLvlInitStd': pLvlInitStd,
+                "MrkvPrbsInit" : np.array(list(init_mrkv_dist)),
+                'Urate_normal' : Urate_normal,
+                'Uspell_normal' : Uspell_normal,
+                'UBspell_normal' : UBspell_normal,
+                'num_base_MrkvStates' : num_base_MrkvStates,
+                'UpdatePrb' : 1.0,
+                'Splurge' : Splurge,
+                'track_vars' : [],
+                'EducType': 0
+                }
+
+adj_highschool = {
+    'pLvlInitMean': pLvlInitMeanH,
+    'EducType' : 1}
+init_highschool = init_dropout.copy()
+init_highschool.update(adj_highschool)
+
+adj_college = {
+    'pLvlInitMean': pLvlInitMeanC,
+    'EducType' : 2}
+init_college = init_dropout.copy()
+init_college.update(adj_college)
+
+# Define a dictionary to represent the baseline scenario
+base_dict = {'shock_type' : "base",
+             'UpdatePrb' : 1.0,
+             'Splurge' : Splurge
+             }
+
+frictionless_changes = {
+             'UpdatePrb' : 1.0
+             }
+
+# Parameters for AggregateDemandEconomy economy
+intercept_prev = np.ones((num_base_MrkvStates,num_base_MrkvStates ))    # Intercept of aggregate savings function
+slope_prev = np.zeros((num_base_MrkvStates,num_base_MrkvStates ))       # Slope of aggregate savings function
+ADelasticity = 0.75                                                     # Elasticity of productivity to consumption
+
+num_max_iterations_solvingAD = 30
+convergence_tol_solvingAD = 1E-6
+Cfunc_iter_stepsize       = 1
+
+# Make a dictionary to specify a Cobb-Douglas economy
+init_ADEconomy = {'intercept_prev': intercept_prev,
+                     'slope_prev': slope_prev,
+                     'ADelasticity' : 0.0,
+                     'demand_ADelasticity' : ADelasticity,
+                     'Cfunc_iter_stepsize' : Cfunc_iter_stepsize,
+                     'MrkvArray' : MrkvArray_base,
+                     'num_base_MrkvStates' : num_base_MrkvStates,
+                     "MrkvArray_base" : MrkvArray_base, 
+                     'CgridBase' : CgridBase,
+                     'EconomyMrkvNow_init': 0,
+                     'act_T' : 400
+                     }
