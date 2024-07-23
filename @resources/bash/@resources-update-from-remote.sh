ed6a021ed2570ebed223f210ce07363c66a56a9b
llorracc <git@llorracc.org>
2024-07-23 14:37:29 -0400

    Update update

--- @resources/bash/@resources-update-from-remote.sh
+++ @resources/bash/@resources-update-from-remote.sh
@@ -7,7 +7,9 @@
 # script should live in the bash dir of the @resources dir
 
 # directory of this script
-here="$(realpath $(dirname $0))" # here=/Volumes/Data/Papers/HAFiscal/HAFiscal-Latest
+bash="$(realpath $(dirname $0))" # here=/Volumes/Data/Papers/HAFiscal/HAFiscal-Latest/@resources/bash
+root="$(realpath $bash/..)"
+echo "$root"
 
 # Set the GitHub repository URL and the desired subdirectory
 repo_url="https://github.com/econ-ark/econ-ark-tools.git"
@@ -20,13 +22,14 @@ resources="@resources"
 repo_dirpath="$repo_url_root/$resources"
 
 # Set the destination directory on your macOS computer
-dest_dir="$here/@resources"
+#dest_dir="$here/@resources"
+dest_dir="$root"
 
 # Change its permissions to allow writing
 chmod -Rf u+w "$dest_dir"
 
 # Clone the GitHub repository into the temporary directory
-[[ -e /tmp/@resources ]] && rm -rf /tmp/econ-ark-tools
+[[ -e /tmp/econ-ark-tools ]] && rm -rf /tmp/econ-ark-tools
 git clone --depth 1 "$repo_url" /tmp/econ-ark-tools
 
 # Navigate to the desired subdirectory within the cloned repository
@@ -37,19 +40,14 @@ src_dir="/tmp/econ-ark-tools/$repo_subdir"
 #echo rsync -avh --delete --checksum --itemize-changes --out-format='"%i %n%L"' "$(realpath .)" "$dest_dir"
 #src_dir="$temp_dir/$repo_subdir"
 
-echo '' ; echo rsync "$src_dir/" "$dest_dir"
-echo 'rsync -avh --delete --checksum --itemize-changes --out-format="%i %n%L" "$src_dir/" "$dest_dir"'
-rsync --dry-run -avh --delete --exclude '.DS_Store' --checksum --itemize-changes --out-format="%i %n%L" "$src_dir/" "$dest_dir" 
-say stop
-read answer
+echo '' ; echo rsync "$src_dir/" "$dest_dir" ;echo '' 
+echo 'rsync -avh --delete --checksum --itemize-changes --out-format="%i %n%L"' "$src_dir/" "$dest_dir"
+#rsync --dry-run -avh --delete --exclude='.DS_Store' --exclude='auto' --exclude='*~' --checksum --itemize-changes --out-format="%i %n%L" "$src_dir/" "$dest_dir"  | grep '^>f.*c' 
 
-rsync -avh --delete --checksum --itemize-changes --out-format="%i %n%L" "$src_dir/" "$dest_dir" | grep '^>f.*c' | tee >(awk 'BEGIN {printf "\n"}; END { if (NR == 0) printf "no files were changed\n\n"; else printf "files were changed\n\n"}')
+rsync            -avh --delete --exclude='.DS_Store' --exclude='auto' --exclude='*~' --checksum --itemize-changes --out-format="%i %n%L" "$src_dir/" "$dest_dir" | grep '^>f.*c' | tee >(awk 'BEGIN {printf "\n"}; END { if (NR == 0) printf "no file(s) changed\n\n"; else printf "file(s) changed\n\n"}')
 
-# # Remove the temporary directory
-# rm -rf "$temp_dir"
-
-# # Return to the source directory
-# popd 
+# Remove the temporary directory
+rm -rf "$temp_dir"
 
 # Change to read-only; edits should be done upstream
 chmod u-w "$dest_dir"
