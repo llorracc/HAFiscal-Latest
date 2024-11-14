944369c0b659d0b22686eb51edff22e8cbae84d0
llorracc <git@llorracc.org>
2024-11-13 23:57:18 -0500

    Update docs responses-cites

--- docs/Private/Submissions/QE/Reports_round_1_replies/responses-cites.sh
+++ docs/Private/Submissions/QE/Reports_round_1_replies/responses-cites.sh
@@ -4,20 +4,20 @@ file=responses-cites-bib
 
 # Find all cite commands and extract them
 grep -oh '\\cite[a-zA-Z]*{[^}]*}' *.tex | \
-# Replace all variations of \cite with simple \cite
-sed 's/\\cite[a-zA-Z]*/\\cite/' | \
-# Remove duplicates
-sort -u | \
-# Replace \cite with \nocite
-sed 's/\\cite/\\nocite/' > responses-cites.tex
+    # Replace all variations of \cite with simple \cite
+    sed 's/\\cite[a-zA-Z]*/\\cite/' | \
+	# Remove duplicates
+    sort -u | \
+	# Replace \cite with \nocite
+    sed 's/\\cite/\\nocite/' > responses-cites.tex
 
 pdflatex responses-cites-bib
 bibtex   responses-cites-bib
-		 syslib="$(kpsewhich system.bib)"
-		 addlib="$(kpsewhich $file-Add-Refs.bib)"
-		 cmd="bibtool -x $file.aux -o $file.bib $syslib $addlib"
-		 echo "$cmd"
-		 eval "$cmd"
+syslib="$(kpsewhich system.bib)"
+addlib="$(kpsewhich $file-Add-Refs.bib)"
+cmd="bibtool -x $file.aux -o $file.bib $syslib $addlib"
+echo "$cmd"
+eval "$cmd"
 pdflatex responses-cites-bib
 pdflatex responses-cites-bib
 
