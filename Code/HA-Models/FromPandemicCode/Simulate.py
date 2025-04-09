330cebb134d9ab6f075fae07d69035e339bb77c5
Kenneth Eva <kennethjeva@gmail.com>
2025-04-09 15:04:06 -0400

    making friendlier directory message

--- Code/HA-Models/FromPandemicCode/Simulate.py
+++ Code/HA-Models/FromPandemicCode/Simulate.py
@@ -44,7 +44,7 @@ def Simulate(Run_Dict,figs_dir,Parametrization='Baseline'):
     try:
         os.mkdir(figs_dir)
     except OSError:
-        print ("Creation of the directory %s failed" % figs_dir)
+        print ("Directory %s already exists" % figs_dir)
     else:
         print ("Successfully created the directory %s " % figs_dir)
     
